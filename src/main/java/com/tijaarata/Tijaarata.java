75cd0588ca4c95d713da6edfa752659f29d0df87
vijay itagikar <itagikarv@gmail.com>
2020-07-02 20:48:55 +0530

    PersistanceContext added

--- src/main/java/com/tijaarata/Tijaarata.java
+++ src/main/java/com/tijaarata/Tijaarata.java
@@ -1,20 +1,12 @@
 package com.tijaarata;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.support.SimpleCacheManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
 @EnableCaching
 @EnableWebSecurity
 //@EnableJdbcHttpSession
