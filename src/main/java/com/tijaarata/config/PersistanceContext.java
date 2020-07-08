4c4a7fcdc8651981e62d0727fcbe8971da7038ee
vijay itagikar <itagikarv@gmail.com>
2020-07-08 12:21:42 +0530

    class name added

--- src/main/java/com/tijaarata/config/PersistanceContext.java
+++ src/main/java/com/tijaarata/config/PersistanceContext.java
@@ -34,6 +34,7 @@ public class PersistanceContext {
 	@Bean(destroyMethod = "close")
 	DataSource dataSource() {
 		HikariConfig dataSourceConfig = new HikariConfig();
+		dataSourceConfig.setDriverClassName(env.getRequiredProperty("spring.datasource.driverClassName"));
 		dataSourceConfig.setJdbcUrl(env.getRequiredProperty("spring.datasource.url"));
 		dataSourceConfig.setUsername(env.getRequiredProperty("spring.datasource.username"));
 		dataSourceConfig.setPassword(env.getRequiredProperty("spring.datasource.password"));
